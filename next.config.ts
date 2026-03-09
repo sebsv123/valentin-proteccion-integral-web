e6598eabee438aaa67b74c2c9537dc5c6fca1517
sebsv123 <sebasifo@ucm.es>
2026-03-09 01:22:45 +0100

    fix: clean vercel deploy config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
