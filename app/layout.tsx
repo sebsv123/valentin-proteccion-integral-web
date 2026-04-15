23a9fd667aa1957f760db7bf58f99ad7a97e4dee
Sebastián Sifontes Valentín <145695928+sebsv123@users.noreply.github.com>
2026-04-15 19:29:34 +0200

    feat: añadir Meta Pixel base code (ID: 1307875004562255)

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,7 @@ const montserrat = Montserrat({ subsets: ['latin'], variable: '--font-body', wei
 
 const clarityId = process.env.NEXT_PUBLIC_CLARITY_ID;
 const GA_ID = 'G-TG4JSVQR5Q';
+const META_PIXEL_ID = '1307875004562255';
 
 import { BackgroundWrapper } from "@/components/background-wrapper";
 import ClickSpark from "@/components/ui/click-spark";
@@ -86,6 +87,30 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             gtag('config', '${GA_ID}');
           `}
         </Script>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${META_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
       </head>
       <body className={`${montserrat.variable} ${playfair.variable} antialiased font-sans`}>
         <BackgroundWrapper />
