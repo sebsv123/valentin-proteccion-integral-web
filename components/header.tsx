8b541a57188b0a3fc206aca8b0b2a8c5dae84501
sebsv123 <sebasifo@ucm.es>
2026-04-18 00:22:37 +0200

    fix: logo webp contenedor ampliado - evitar recorte

--- components/header.tsx
+++ components/header.tsx
@@ -14,8 +14,8 @@ import IridescenceBackground from './ui/iridescence-background';
 function Brand() {
   return (
     <Link href="/" className="flex items-center gap-3 shrink-0">
-      <MetallicPaint className="relative h-[54px] w-[200px] max-w-[60vw] rounded-[20px] border border-white/20 bg-white shadow-sm sm:w-[240px]">
-        <Image src="/brand/logo-vpi.webp" alt={site.name} width={240} height={54} className="object-contain p-1.5" priority />
+      <MetallicPaint className="relative h-[48px] w-[240px] max-w-[65vw] overflow-hidden rounded-[20px] border border-white/20 bg-white p-1 shadow-sm sm:w-[300px]">
+        <Image src="/brand/logo-vpi.webp" alt={site.name} width={300} height={48} className="object-contain object-left" priority />
       </MetallicPaint>
     </Link>
   );
