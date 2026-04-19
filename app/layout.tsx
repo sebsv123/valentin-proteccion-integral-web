45fe7e701db368836f30aaf7936eaab4397f852c
sebsv123 <sebasifo@ucm.es>
2026-04-19 23:20:25 +0200

    fix(build): style prop del Meta Pixel noscript como objeto JSX en lugar de string CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,7 +108,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           <img
             height="1"
             width="1"
-            style="display:none"
+            style={{ display: 'none' }}
             src={`https://www.facebook.com/tr?id=${META_PIXEL_ID}&ev=PageView&noscript=1`}
             alt=""
           />
