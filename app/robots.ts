a3ca9670bf6ef9626a6840cd13989942eba5c398
sebsv123 <sebasifo@ucm.es>
2026-04-24 02:36:51 +0200

    fix: remove /landing from robots.txt disallow to allow Google crawling

--- app/robots.ts
+++ app/robots.ts
@@ -7,7 +7,6 @@ export default function robots(): MetadataRoute.Robots {
         userAgent: '*',
         allow: '/',
         disallow: [
-          '/landing',
           '/gracias',
           '/api/',
           '/_next/',
